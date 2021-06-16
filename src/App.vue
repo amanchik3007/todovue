11a6dd493962f186bb44b9e64e168fc63efe07d1
Aman <a.ergali@nomad.kz>
2021-06-16 15:23:00 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg"/>
+    <input type="text" v-model="msg">
   </div>
 </template>
 
@@ -12,6 +13,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      msg: ""
+    }
   }
 }
 </script>
